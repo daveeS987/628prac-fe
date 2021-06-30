9c1354abbd15b7f60502d205f610918ba5673d33
Davee Sok <daveesok@gmail.com>
2021-06-30 15:16:56 -0500

    added prettier configs

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Head from 'next/head';
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Head from "next/head";
 
 function Layout({ children }) {
   return (
     <>
       <Head>
         <title>NextJS Virtual Store</title>
-        <meta name="Davee's Virtual Store" content='A mock virutal store made with next js capable of performing crud opertations to a Mongo database' />
+        <meta
+          name="Davee's Virtual Store"
+          content="A mock virutal store made with next js capable of performing crud opertations to a Mongo database"
+        />
       </Head>
-      <h1>Layout for now</h1>
       <Header />
+      <h1>Layout for now</h1>
       {children}
       <Footer />
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Layout from '../components/Layout/Layout';
-import SelectCategories from '../components/SelectCategories/SelectCategories';
-import Products from '../components/Products/Products';
+import Layout from "../components/Layout/Layout";
+import SelectCategories from "../components/SelectCategories/SelectCategories";
+import Products from "../components/Products/Products";
 
 export default function Home() {
   return (
