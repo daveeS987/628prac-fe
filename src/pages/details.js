a26a5a82141e5d011ea56e208c7c949298d81f31
Davee Sok <daveesok@gmail.com>
2021-07-12 17:18:58 -0500

    committing because forgot to check out into another branch

---
+++ src/pages/details.js
@@ -0,0 +1,18 @@
+import Layout from '../components/Layout/Layout';
+
+function Details() {
+  return (
+    <>
+      <Layout>
+        <h1>Product Details Page for now</h1>
+        <h3>Product Name</h3>
+        <h3>description</h3>
+        <h3>imageUrl</h3>
+        <h3>price</h3>
+        <h3>inStock</h3>
+      </Layout>
+    </>
+  );
+}
+
+export default Details;
