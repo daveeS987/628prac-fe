e0633c71e9ef33715546c6fcd1e0a1f98a9ccaf9
Davee Sok <daveesok@gmail.com>
2021-07-29 00:46:22 -0500

    cleaned up some css in checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,39 +1,87 @@
 import React from 'react';
 import { signIn, signOut, useSession } from 'next-auth/client';
+import { Typography, makeStyles, Container, Button } from '@material-ui/core';
 
 import Layout from '../components/Layout/Layout';
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+    flexDirection: 'column',
+  },
+  title: {
+    marginTop: '2rem',
+  },
+  signInButton: {
+    marginTop: '2rem',
+    textAlign: 'center',
+    marginLeft: '13rem',
+  },
+}));
 
 function CheckoutPage() {
   const [session, loading] = useSession();
 
+  const classes = useStyles();
+
   return (
     <>
       <Layout>
-        <h1>Checkout Page</h1>
+        <Typography className={classes.title} variant="h3" align="center">
+          Checkout Page
+        </Typography>
 
         {!session && (
           <>
-            <h2>Sign in to see Checkout Page</h2>
-            <h3>This uses sessions to see if you're logged in</h3>
-            <br />
-            <button onClick={() => signIn()}>Sign in</button>
+            <div className={classes.heroContent}>
+              <Container maxWidth="sm">
+                <Typography variant="h5" align="center" color="textSecondary">
+                  Sign in to go to Checkout Page
+                </Typography>
+                <Typography variant="h6" align="center" color="textSecondary">
+                  This uses sessions to see if you're logged in
+                </Typography>
+                <Button
+                  className={classes.signInButton}
+                  size="large"
+                  variant="contained"
+                  color="primary"
+                  onClick={() => signIn()}
+                >
+                  Sign in
+                </Button>
+              </Container>
+            </div>
           </>
         )}
         {session && (
-          <>
-            <h3>
-              This is a protected route. You can only see this, if you're logged
-              in.
-            </h3>
-            <h3>Signed in as {session.user.email}</h3> <br />
-            <button
-              onClick={() =>
-                signOut({ callbackUrl: `${process.env.NEXTAUTH_URL}` })
-              }
-            >
-              Sign out
-            </button>
-          </>
+          <div className={classes.heroContent}>
+            <Container maxWidth="sm">
+              <Typography variant="h5" align="center" color="textSecondary">
+                This is a protected route. You can only see this, if you're
+                logged in.
+              </Typography>
+              <br />
+              <Typography variant="h6" align="center" color="textSecondary">
+                Signed in as {session.user.email}
+              </Typography>
+              <Button
+                className={classes.signInButton}
+                size="large"
+                variant="contained"
+                color="primary"
+                onClick={() =>
+                  signOut({ callbackUrl: `${process.env.NEXTAUTH_URL}` })
+                }
+              >
+                Sign Out
+              </Button>
+            </Container>
+          </div>
         )}
       </Layout>
     </>
