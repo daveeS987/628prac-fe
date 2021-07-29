3ded59c546c1673bd757775a51725ccf66fead56
Davee Sok <daveesok@gmail.com>
2021-07-29 15:07:42 -0500

    updated color

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,6 +3,23 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Head from 'next/head';
 
+import { ThemeProvider } from '@material-ui/styles';
+import { createTheme } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
+import orange from '@material-ui/core/colors/orange';
+import blue from '@material-ui/core/colors/blue';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[700],
+    },
+    secondary: {
+      main: orange[400],
+    },
+  },
+});
+
 function Layout({ children }) {
   return (
     <>
@@ -13,9 +30,11 @@ function Layout({ children }) {
           content="A mock virutal store made with next js capable of performing crud opertations to a Mongo database"
         />
       </Head>
-      <Header />
-      {children}
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <Header />
+        {children}
+        <Footer />
+      </ThemeProvider>
     </>
   );
 }
